483e906dc67d3d13cb0f92798cb568c0e1a1cad4
nigel2000 <d.jones560@ntlworld.com>
2023-06-23 18:39:56 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,42 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+  
 export class AppComponent {
   title = 'todo-list';
+  tasks: Task[] = [
+    new Task("Vist Ann"),
+    new Task("Call Dad"),
+    new Task("Go to the gym"),
+    new Task("Wash the dishes"),
+    new Task("Go to the party")
+  ] 
+
+  add(newTask: string) {
+    this.tasks.push(new Task(newTask))
+  }
+
+  remove(existingTask: Task) {
+    var userConfirmed = confirm(`Are you sure you want to remove the following task? \n "${existingTask.title}"`)
+    
+    if (userConfirmed) {
+      this.tasks = this.tasks.filter(task => task != existingTask)
+    } 
+  }
+
+  
+}
+
+class Task {
+
+  constructor(public title: string) {
+
+  }
+
+  toggleIsDone() {
+    this.isDone = !this.isDone;
+  }
+
+  public isDone = false;
+
 }
